2627b912af141368b3186110eace8eaccd140214
Vitaliy Lyalin <vitaliy.lyalin@gmail.com>
2022-09-28 21:46:10 +0300

     Додає адаптивну верстку та підключення адаптивної графіки для header mobile-menu, hero

--- js/menu.js
+++ js/menu.js
@@ -4,11 +4,11 @@
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
   };
-​
-  refs.openMenuBtn.addEventListener('click', toggleModal);
-  refs.closeMenuBtn.addEventListener('click', toggleModal);
-​
-  function toggleModal() {
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
     refs.menu.classList.toggle('is-open');
   }
 })();
